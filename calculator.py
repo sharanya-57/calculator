3d5179e48a3478bcf9b0143d21ef6f9fd37c6e9a
sharanya-57 <sharanyadhyasani@gmail.com>
2026-02-14 11:23:16 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,12 @@
 import sys
 n1 = int(sys.argv[1])
-n2 = int(sys.argv[2))
-r1=n1+n2
-r2=n1-n2
-r3=n1*n2
-r4=n1/n2
+n2 = int(sys.argv[2])
+r1 = n1+n2
+r2 = n1-n2
+r3 = n1*n2
+r4 = n1/n2
 print("Addition",r1)
 print("Subtraction",r2)
 print("Multiplication",r3)
 print("Division",r4)
-print("Changes are made in python files")
-
-
+print("Changes are made in python file")
